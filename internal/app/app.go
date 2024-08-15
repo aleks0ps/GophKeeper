45161adfc9d37c136e86b61cc743027b35b40f82
alex0ps <alexey.petrunev@yandex.ru>
2024-08-15 03:24:58 +0300

    add files

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,20 @@
 package app
 
-import "fmt"
+import (
+	"net/http"
+
+	svc "github.com/aleks0ps/GophKeeper/internal/app/service"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
 
 func Run() {
-	fmt.Println("Hello")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Post("/register", svc.Register)
+	r.Post("/login", svc.Login)
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("OK"))
+	})
+	http.ListenAndServe(":8080", r)
 }
