fa3a1a3bf5b3d31b140f56b43f14ba0092ec25e7
alex0ps <alexey.petrunev@yandex.ru>
2024-12-18 05:15:31 +0300

    add more files

--- cmd/gophkeeper/config/config.go
+++ cmd/gophkeeper/config/config.go
@@ -16,21 +16,21 @@ const (
 type Config struct {
 	RunAddress  string `env:"RUN_ADDRESS"`
 	DatabaseURI string `env:"DATABASE_URI"`
-	SecretKey   string `env:"SECRET_KEY"`
+	Secret      string `env:"SECRET_KEY"`
 }
 
 func ParseOptions() *Config {
 	opts := Config{
 		RunAddress:  defaultRunAddress,
 		DatabaseURI: defaultDatabaseURI,
-		SecretKey:   defaultSecretKey,
+		Secret:      defaultSecretKey,
 	}
 	if err := env.Parse(&opts); err != nil {
 		fmt.Println("failed:", err)
 	}
 	flag.StringVar(&opts.RunAddress, "l", opts.RunAddress, "Listen address:port")
 	flag.StringVar(&opts.DatabaseURI, "d", opts.DatabaseURI, "Postgres connection string")
-	flag.StringVar(&opts.DatabaseURI, "s", opts.SecretKey, "Secret key for data encryption")
+	flag.StringVar(&opts.Secret, "s", opts.Secret, "Secret key for data encryption")
 	flag.Parse()
 	return &opts
 }
