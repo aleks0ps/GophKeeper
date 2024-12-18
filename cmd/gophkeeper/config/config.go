b619862a5b9bf731d9758ba31f9971ebade21190
alex0ps <alexey.petrunev@yandex.ru>
2024-12-18 05:10:29 +0300

    add more files

--- cmd/gophkeeper/config/config.go
+++ cmd/gophkeeper/config/config.go
@@ -10,23 +10,27 @@ import (
 const (
 	defaultRunAddress  = "localhost:8080"
 	defaultDatabaseURI = "postgres://gophkeeper:gophkeeper@localhost:5432/gophkeeper?sslmode=disable"
+	defaultSecretKey   = "password"
 )
 
 type Config struct {
 	RunAddress  string `env:"RUN_ADDRESS"`
 	DatabaseURI string `env:"DATABASE_URI"`
+	SecretKey   string `env:"SECRET_KEY"`
 }
 
 func ParseOptions() *Config {
 	opts := Config{
 		RunAddress:  defaultRunAddress,
 		DatabaseURI: defaultDatabaseURI,
+		SecretKey:   defaultSecretKey,
 	}
 	if err := env.Parse(&opts); err != nil {
 		fmt.Println("failed:", err)
 	}
 	flag.StringVar(&opts.RunAddress, "l", opts.RunAddress, "Listen address:port")
 	flag.StringVar(&opts.DatabaseURI, "d", opts.DatabaseURI, "Postgres connection string")
+	flag.StringVar(&opts.DatabaseURI, "s", opts.SecretKey, "Secret key for data encryption")
 	flag.Parse()
 	return &opts
 }
